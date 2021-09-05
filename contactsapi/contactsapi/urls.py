a1287ac20939d9024876ae561d4e415b36d93e1a
Zubair Ahmed Anas <zubairahmed.anas@bracits.com>
2021-09-05 21:31:12 +0600

    Zubair | contactsapi | url added

--- contactsapi/contactsapi/urls.py
+++ contactsapi/contactsapi/urls.py
@@ -16,8 +16,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+...
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+...
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Contact List Api",
+        default_version='v1',
+        description="api for contact",
+        terms_of_service="https://dreamsco/terms/",
+        contact=openapi.Contact(email="contact@contact.remote"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/contact/', include('contact.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("redoc", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
